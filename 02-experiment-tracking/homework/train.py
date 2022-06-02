4e829e0eed6b3763586df5624128896bdbe7804b
bengsoon <bengsoonchuah@gmail.com>
2022-06-02 10:35:45 +0800

    Week 2 Homework Solution

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -5,6 +5,7 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -13,17 +14,28 @@ def load_pickle(filename: str):
 
 def run(data_path):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+    # start mlflow
+    with mlflow.start_run():
+        # use autolog
+        mlflow.sklearn.autolog()
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
+
+    # Setting the tracking_uri for mlflow
+    mlflow.set_tracking_uri("sqlite:///..//mlflow.db")
+
+    # Create a new experiment for green_taxi
+    mlflow.set_experiment("green-taxi_experiment")
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
